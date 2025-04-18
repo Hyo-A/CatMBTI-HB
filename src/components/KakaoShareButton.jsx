b381d58aa683c5cb04b943b396ea3b2bd07ba1a8
Hyo-A <janggydk@daum.net>
2025-04-18 12:20:45 +0900

    fourthcommit

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -12,73 +12,31 @@ const KakaoShareButton = () => {
     Kakao.cleanup();
     // 앞단의 캐쉬값 초기화
     Kakao.init("187e3c94136aa97c89299bab5e0822c2");
-    // 이 값으로 초기화되는거
+    // 내 키값으로 초기화되는거
+    Kakao.isInitialized();
+    // 초기화 잘 되었는지 확인해주는거
   }, []);
 
   const sharekakao = () => {
     Kakao.Share.sendDefault({
       objectType: "feed",
       content: {
-        title: "오늘의 디저트",
-        description: "아메리카노, 빵, 케익",
+        title: "예비 집사 판별기 결과",
+        description:
+          "예비 집사님이 고양이를 키운다면, 잘 맞는 고양이는 코숏 입니다.",
         imageUrl:
           "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
+          mobileWebUrl: resultURL,
+          webUrl: resultURL,
         },
-      },
-      itemContent: {
-        profileText: "Kakao",
-        profileImageUrl:
-          "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-        titleImageUrl:
-          "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-        titleImageText: "Cheese cake",
-        titleImageCategory: "Cake",
-        items: [
-          {
-            item: "Cake1",
-            itemOp: "1000원",
-          },
-          {
-            item: "Cake2",
-            itemOp: "2000원",
-          },
-          {
-            item: "Cake3",
-            itemOp: "3000원",
-          },
-          {
-            item: "Cake4",
-            itemOp: "4000원",
-          },
-          {
-            item: "Cake5",
-            itemOp: "5000원",
-          },
-        ],
-        sum: "총 결제금액",
-        sumOp: "15000원",
-      },
-      social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
       },
       buttons: [
         {
-          title: "웹으로 이동",
+          title: "나도 테스트 하러가기",
           link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
-          },
-        },
-        {
-          title: "앱으로 이동",
-          link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
+            mobileWebUrl: url,
+            webUrl: url,
           },
         },
       ],
